46c3ceee6b0a9e48056895823074d52f8d118e2b
Mikely Fernanda <mikelyfernanda93@gmail.com>
2024-07-30 01:14:07 -0300

    Últimos ajustes no Projeto-FINAL

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from Bio import SeqIO, Seq
 from bio.ler_fasta import ler_fasta
-from bio.constantes import DNA_PARA_AMINOACIDO
-from bio.constantes import DNA_STOP_CODONS
 from bio.sequencia import Sequencia
+from problemas.problema_3 import sequencias_mutadas
 
 
 arquivo_fasta = 'arquivos/Flaviviridae-genomes.fasta'
@@ -12,27 +10,36 @@ seq_unica = sequencias[1].sequencia
 #print(seq_unica)
 
 # Testando PROBLEMA-2 => 
-sequencia_classe = Sequencia(sequencias[1].nome, seq_unica) 
-#resultado_classe = sequencia_classe.traducao(stop_codon=False)
-#print(resultado_classe)
+sequencia_classe = Sequencia(seq_unica) 
+resultado_Problema_2_F = sequencia_classe.traducao(to_stop=False)
+resultado_Problema_2_T = sequencia_classe.traducao(to_stop=True)
+#print(f'\nResultado do PROBLEMA-2 com o atributo To_Stop FALSE =>\n\n{resultado_Problema_2_F}')
+#print(f'\nResultado do PROBLEMA-2 com o atributo To_Stop TRUE =>\n\n{resultado_Problema_2_T}')
+
 # ---------------------------------------------------------------
 
-# Testando Metodo TRANSCRICAO => 
-resultado_classe = sequencia_classe.transcricao()
-#print(resultado_classe)
+pequena_sequencia = Sequencia("ACTGAAGCT")
+
+# Testando Método TRANSCRICAO() => 
+resultado_Metodo_Transcricao = seq_unica.transcricao()
+#print(f'\nResultado do Método transcricao() =>\n{resultado_Metodo_Transcricao}')
+print(f'\nResultado do Método transcricao() na Sequencia Pequena [ACTGAAGCT] =>\n{pequena_sequencia.transcricao()}')
+
+# Testando Método SEQUENCIA_COMPLEMENTAR() => 
+resultado_Metodo_SequenciaComplementar = seq_unica.sequencia_complementar()
+#print(f'\nResultado do Método sequencia_complementar() =>\n{resultado_Metodo_SequenciaComplementar}')
+print(f'\nResultado do Método sequencia_complementar() na Sequencia Pequena [ACTGAAGCT] =>\n{pequena_sequencia.sequencia_complementar()}')
+
+
+# Testando Método SEQUENCIA_COMPLEMENTAR_REVERSA() => 
+resultado_Metodo_SequenciaComplementarReversa = seq_unica.sequencia_complementar_reversa()
+#print(f'\nResultado do Método sequencia_complementar_reversa() =>\n{resultado_Metodo_SequenciaComplementarReversa}')
+print(f'\nResultado do Método sequencia_complementar_reversa() na Sequencia Pequena [ACTGAAGCT] =>\n{pequena_sequencia.sequencia_complementar_reversa()}')
+
 
 # ---------------------------------------------------------------
 
 # Testando PROBLEMA-3 => 
+sequencias_mutadas(sequencias)
 
-# ---------------------------------------------------------------
 
-# Testando Metodos de sequencias => 
-objeto_sequencia = Sequencia(sequencias[1].nome, seq_unica)
-resultado_sequencia_complementar = objeto_sequencia.sequencia_complementar()
-#print("SEQUENCIA COMPLEMENTAR => ",resultado_sequencia_complementar)
-
-resultado_sequencia_complementar_reversa = objeto_sequencia.sequencia_complementar_reversa()
-print("NOME DO ORGANISMO => ", objeto_sequencia.nome)
-print("\n SEQUENCIA COMPLEMENTAR REVERSA => ", resultado_sequencia_complementar_reversa)
-print("PASSOU NA REVERSA!!!")
